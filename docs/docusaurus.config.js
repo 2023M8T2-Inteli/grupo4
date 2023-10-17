cb5a839437730f454f32e4ef73941c5b94d5f7dd
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-10-17 15:57:13 -0300

    feat: adding docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Docs | Grupo4',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/grupo4',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
