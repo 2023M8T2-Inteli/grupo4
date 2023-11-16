36d50ab9ddab8a132bfff6bf2f2f93e97c53f111
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-11-16 15:50:49 -0300

    fix: video demo

--- docs/src/components/VideoPlayer/VideoPlayer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ReactPlayer } from 'react-player';
-
-const VideoPlayer = ({ url }) => (
-  <ReactPlayer playing controls url={url} />
-);
-
-export default VideoPlayer;
