13a48d6f8bd384263eb1a8999d9f0556234015c2
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-11-29 02:06:59 -0300

    docs: Add logs section to solution development documentation

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -20,37 +20,60 @@ const sidebars = {
 
   tutorialSidebar: [
     {
-      type: 'doc',
-      label: 'Introdução',
-      id: 'intro',
+      type: "doc",
+      label: "Introdução",
+      id: "intro",
     },
     {
-      type: 'category',
-      label: 'Contexto da Indústria',
-      items: ['Contexto da Indústria/1.1 Análise de Impacto Ético', 'Contexto da Indústria/1.2 TAM-SAM-SOM','Contexto da Indústria/1.3 Análise Financeira','Contexto da Indústria/1.4 Matriz de Riscos', 'Contexto da Indústria/1.5 Matriz de Oceano Azul'],
+      type: "category",
+      label: "Contexto da Indústria",
+      items: [
+        "Contexto da Indústria/1.1 Análise de Impacto Ético",
+        "Contexto da Indústria/1.2 TAM-SAM-SOM",
+        "Contexto da Indústria/1.3 Análise Financeira",
+        "Contexto da Indústria/1.4 Matriz de Riscos",
+        "Contexto da Indústria/1.5 Matriz de Oceano Azul",
+      ],
     },
     {
-      type: 'category',
-      label: 'Proposta de Solução',
-      items: ['Proposta de Solução/2.1 Arquitetura da Solução', 'Proposta de Solução/2.2 Personas','Proposta de Solução/2.3 Histórias de Usuário', 'Proposta de Solução/2.4 Levantamento de Requisitos', 'Proposta de Solução/2.5 Canvas Proposta de Valor','Proposta de Solução/2.6 Jornada do Usuário'],
+      type: "category",
+      label: "Proposta de Solução",
+      items: [
+        "Proposta de Solução/2.1 Arquitetura da Solução",
+        "Proposta de Solução/2.2 Personas",
+        "Proposta de Solução/2.3 Histórias de Usuário",
+        "Proposta de Solução/2.4 Levantamento de Requisitos",
+        "Proposta de Solução/2.5 Canvas Proposta de Valor",
+        "Proposta de Solução/2.6 Jornada do Usuário",
+      ],
     },
     {
-      type: 'category',
-      label: 'Desenvolvimento da Solução',
-      items: ['Desenvolvimento da Solução/3.1 Design Patterns','Desenvolvimento da Solução/3.2 Mapeamento e navegação', 'Desenvolvimento da Solução/3.3 Chatbot', 'Desenvolvimento da Solução/3.4 Banco de Dados', 'Desenvolvimento da Solução/3.5 LLM', 'Desenvolvimento da Solução/3.6 Logs', 'Desenvolvimento da Solução/3.7 WebSocket'],
+      type: "category",
+      label: "Desenvolvimento da Solução",
+      items: [
+        "Desenvolvimento da Solução/3.1 Design Patterns",
+        "Desenvolvimento da Solução/3.2 Mapeamento e navegação",
+        "Desenvolvimento da Solução/3.3 Chatbot",
+        "Desenvolvimento da Solução/3.4 Banco de Dados",
+        "Desenvolvimento da Solução/3.6 Logs",
+        "Desenvolvimento da Solução/3.5 LLM",
+        "Desenvolvimento da Solução/3.7 WebSocket",
+      ],
     },
     {
-      type: 'doc',
-      label: 'Sprint Review',
-      id: 'Apresentações',
+      type: "doc",
+      label: "Sprint Review",
+      id: "Apresentações",
     },
     {
-      type: 'category',
-      label: 'Referências',
-      items: ['Referências/Análise de mercado', 'Referências/Análise Financeira'],
+      type: "category",
+      label: "Referências",
+      items: [
+        "Referências/Análise de mercado",
+        "Referências/Análise Financeira",
+      ],
     },
   ],
-
 };
 
 module.exports = sidebars;
