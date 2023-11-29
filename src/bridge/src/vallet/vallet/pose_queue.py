b200e419a063e633297009f1a0cde07d4cef6c04
Henrique Marlon <henriquemarloncs@gmail.com>
2023-11-29 02:04:23 -0300

    refactor: :zap: refactoring streamer module and pose queue

--- src/bridge/src/vallet/vallet/pose_queue.py
+++ src/bridge/src/vallet/vallet/pose_queue.py
@@ -1,12 +1,18 @@
 import rclpy
 from enum import Enum
+from typing import Any
 from queue import Queue
 from rclpy.node import Node
+from .streamer import Streamer
 from vallet_msgs.msg import Log
 from std_msgs.msg import String
 from .publisher import Publisher
 from .subscriber import Subscriber
 from geometry_msgs.msg import Pose
+from .__init__ import socket_client
+from tf_transformations import quaternion_from_euler
+from geometry_msgs.msg import Pose, Point, Quaternion
+
 
 
 class RobotStatus(Enum):
@@ -37,20 +43,47 @@ class PoseQueue(Node):
                                 "/logs", 
                                 Log)
         
+        self.streamer = Streamer(self,
+                                 socket_client,
+                                 "/enqueue",
+                                 None,
+                                 self.enqueue_pose)
+        
         self.enqueue.create_sub(self.enqueue_pose)
         self.status.create_sub(self.status_callback)
 
         self.queue = Queue()
         self.robot_status = RobotStatus.FREE
 
-    def enqueue_pose(self, msg) -> None:
-        log_msg = Log(node_name=self.enqueue.topic_name, 
-                      action=f'{msg}',
-                      unix_time=int(self.get_clock().now().to_msg().sec))
-        self.logger.publish(log_msg)
+    def enqueue_pose(self, msg: Any) -> None:
+        self.get_logger().info(f"Enqueuing {msg} on {self.enqueue.topic_name}")
+        if isinstance(msg, Pose):
+            log_msg = Log(node_name=self.enqueue.topic_name, 
+                        action=f'{msg}',
+                        unix_time=int(self.get_clock().now().to_msg().sec))
+            self.logger.publish(log_msg)
 
-        self.queue.put(msg)
+            self.queue.put(msg)
 
+        elif isinstance(msg, dict):
+            try:
+                x = msg["x"]
+                y = msg["y"]
+                q_x, q_y, q_z, q_w = quaternion_from_euler(x, y, 0.0)
+                pose = Pose(
+                    position=Point(x=x, y=y, z=0.0),
+                    orientation=Quaternion(x=q_x, y=q_y, z=q_z, w=q_w)
+                )
+
+                log_msg = Log(node_name=self.enqueue.topic_name, 
+                        action=f'{msg}',
+                        unix_time=int(self.get_clock().now().to_msg().sec))
+                self.logger.publish(log_msg)
+
+                self.queue.put(pose)
+            except ValueError:
+                self.get_logger().info("Invalid pose.")
+                
     def status_callback(self, msg: String) -> None:
         self.robot_status = RobotStatus.FREE if msg.data == "FREE" else RobotStatus.BUSY
 
