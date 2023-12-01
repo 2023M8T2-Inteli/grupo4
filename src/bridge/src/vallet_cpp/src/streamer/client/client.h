62c599490d0602a7fb4a4c72cb43ba93363b452c
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-12-01 01:34:47 -0300

    feat(vallet_cpp): socketio initialization working

--- src/bridge/src/vallet_cpp/src/streamer/client/client.h
+++ src/bridge/src/vallet_cpp/src/streamer/client/client.h
@@ -3,14 +3,27 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include <sio_client.h>
+#include <mutex>
+#include <condition_variable>
 
-class Client : public rclcpp::Node
+class ClientStreamer : public rclcpp::Node
 {
-   public:
-      Client();
+public:
+   ClientStreamer(sio::client &client);
+   ~ClientStreamer();
+
+private:
+   sio::client &_client;
+   std::mutex _lock;
+   std::condition_variable_any _cond;
+   bool _connect_finish;
+
+   void _on_connected();
+   void _on_close(sio::client::close_reason const &reason);
+   void _on_fail();
+   void _emit(const char *topic, const std::string &message);
 };
 
 #endif // CLIENTCLASS_H
-
 
 // 10.128.68.115
--- src/bridge/src/vallet_cpp/src/streamer/main.cpp
+++ src/bridge/src/vallet_cpp/src/streamer/main.cpp
@@ -3,7 +3,8 @@
 int main(int argc, char **argv)
 {
    rclcpp::init(argc, argv);
-   auto client_node = std::make_shared<Client>();
+   sio::client my_client;
+   auto client_node = std::make_shared<ClientStreamer>(my_client);
    rclcpp::spin(client_node);
    rclcpp::shutdown();
    return 0;
