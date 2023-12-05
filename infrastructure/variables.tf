7ee45ba312de1808a93307d4694322eb27bc2287
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-12-05 15:05:58 -0300

    feat: update terraform aws keys

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,20 +7,20 @@ variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "AWS Access Key ID"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  #default = "ASIAWUPUSUIEYZEZUYCH"
+  default = "ASIAWUPUSUIEWUALVFM5"
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "AWS Secret Access Key"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  #default = "0EcGH+9nUAnZSaQFNeBlnU33627MCV9YPZI5TGrw"
+  default = "ffDUTg/sZaNxjh9ll9kXaPmI5F4EstWg+JeU0Ilu"
 }
 
 variable "AWS_SESSION_TOKEN" {
   type        = string
   description = "AWS Session Token"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  #default = "FwoGZXIvYXdzEAsaDEB7eQNvQsjnv9D+OSLNAblu0fP8PrzYjPponWEiNGPY1QQMatENkX+Iwz5Gcl9eyMZ3b+FJu2WGAqLEJAIa5BuILrHUyEaOiLDUh6p3/IcaM5KcZWgCfQ2xoBIw+/1pP9MQX+rJ5gAinHR6Uyu2OoKJymRAxGj+fy5jzwxauqnexaX04QLwLqxTO5NH5NTmU/alPJquVCVsci+tY9FX1Mkplpr0J4HTKeybbP6z75r7nLhzBYZarpZjE2n1HfVFbpeAa1O+FviMQbiHWi8TJHIr3Iub80pG/02DsR0ovJK4qwYyLXXUsK2WRh0agCDuft2FiosDHB8bxL51HmNvmvXEY1t5M6bBMVq4tMzPIVUQkg=="
+  default = "FwoGZXIvYXdzECMaDJ8I7kI2v+ayEKiHhSLNARVirOTiPGwewgDLJ4w/YNVP8ySRVYbAHb34bczWd3eayl+EQKj9Tlqt+gCQQ+BPQVa/VcAlb9xKS7WAmAp5Oc5M9Z7YgVsB3MY8L4xOfqj2+7lqGGQwGS+Z569wmhbe+Ulzi0pZ5A+9UqWKbbW4TW8FvheKSjdArbzOMiw5oy/wQb/Lmf5WORIIynVHKZzwDr+XbYhz2vj735SZLvmAX0zVexBzO46YT7zwL3vnpGaov9F0+epKQRYjAUT23cGD4/L4H+rQYIz4rsnYIr8ouca9qwYyLSSWx8Ep07z/USPl7THA71ruNkh+fK1Xq9YqdHselTXGXuZtfONTQjOO/6accQ=="
 }
 
 variable "public_subnet_cidrs" {
