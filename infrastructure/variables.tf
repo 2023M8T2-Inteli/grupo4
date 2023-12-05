bfdc8e7238a4a7a0f5fd3daadc3a42048304dd2b
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-12-05 15:00:26 -0300

    fix: variables aws

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,18 +6,21 @@ variable "AWS_REGION" {
 variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "AWS Access Key ID"
-  default = "ASIAWUPUSUIEYZEZUYCH"
+  //O Padrão default é para subir a infraestrutura na AWS de forma manual
+  #default = "ASIAWUPUSUIEYZEZUYCH"
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "AWS Secret Access Key"
-  default = "0EcGH+9nUAnZSaQFNeBlnU33627MCV9YPZI5TGrw"
+  //O Padrão default é para subir a infraestrutura na AWS de forma manual
+  #default = "0EcGH+9nUAnZSaQFNeBlnU33627MCV9YPZI5TGrw"
 }
 
 variable "AWS_SESSION_TOKEN" {
   type        = string
   description = "AWS Session Token"
-  default = "FwoGZXIvYXdzEAsaDEB7eQNvQsjnv9D+OSLNAblu0fP8PrzYjPponWEiNGPY1QQMatENkX+Iwz5Gcl9eyMZ3b+FJu2WGAqLEJAIa5BuILrHUyEaOiLDUh6p3/IcaM5KcZWgCfQ2xoBIw+/1pP9MQX+rJ5gAinHR6Uyu2OoKJymRAxGj+fy5jzwxauqnexaX04QLwLqxTO5NH5NTmU/alPJquVCVsci+tY9FX1Mkplpr0J4HTKeybbP6z75r7nLhzBYZarpZjE2n1HfVFbpeAa1O+FviMQbiHWi8TJHIr3Iub80pG/02DsR0ovJK4qwYyLXXUsK2WRh0agCDuft2FiosDHB8bxL51HmNvmvXEY1t5M6bBMVq4tMzPIVUQkg=="
+  //O Padrão default é para subir a infraestrutura na AWS de forma manual
+  #default = "FwoGZXIvYXdzEAsaDEB7eQNvQsjnv9D+OSLNAblu0fP8PrzYjPponWEiNGPY1QQMatENkX+Iwz5Gcl9eyMZ3b+FJu2WGAqLEJAIa5BuILrHUyEaOiLDUh6p3/IcaM5KcZWgCfQ2xoBIw+/1pP9MQX+rJ5gAinHR6Uyu2OoKJymRAxGj+fy5jzwxauqnexaX04QLwLqxTO5NH5NTmU/alPJquVCVsci+tY9FX1Mkplpr0J4HTKeybbP6z75r7nLhzBYZarpZjE2n1HfVFbpeAa1O+FviMQbiHWi8TJHIr3Iub80pG/02DsR0ovJK4qwYyLXXUsK2WRh0agCDuft2FiosDHB8bxL51HmNvmvXEY1t5M6bBMVq4tMzPIVUQkg=="
 }
 
 variable "public_subnet_cidrs" {
