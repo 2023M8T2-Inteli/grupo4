a5ec86a9fc29287707e30224d496ceb7d4573d98
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-12-11 23:35:22 -0300

    feat: update terraform

---
+++ infrastructure/terraform.tfvars
@@ -0,0 +1,4 @@
+AWS_REGION = "us-east-1"
+AWS_ACCESS_KEY_ID = "minhaChaveDeAcesso"
+AWS_SECRET_ACCESS_KEY = "minhaChaveSecreta"
+AWS_SESSION_TOKEN = "meuTokenDeSessao"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,26 +1,26 @@
 variable "AWS_REGION" {
   type        = string
   description = "AWS Region"
-  default     = "us-east-1"
+  //default     = "us-east-1"
 }
 variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "AWS Access Key ID"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  default = "ASIAWUPUSUIE3SFW677C"
+  //default = ""
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "AWS Secret Access Key"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  default = "2DghTTTuyF+h06m2iGFw9mt5+bopVOxQDdR3+9Eg"
+  //default = ""
 }
 
 variable "AWS_SESSION_TOKEN" {
   type        = string
   description = "AWS Session Token"
   //O Padrão default é para subir a infraestrutura na AWS de forma manual
-  default = "FwoGZXIvYXdzEFMaDI5zwTzt+LDOseUPhCLNAYXUYEyIdnF0Snss57ho7gzIZ5HUnSbL0fAZHj/xKUWBa1P88ptme7IP7ebkE+KWiSunTY8QKQZVv/0K0b8lCDGWuMf2EhS+Rp3jO/PI+ut7ni+ef9PK4wiKYY/NH4numQRCQdzPTSIKfSWaDLgO8JIDTFQsIJf6clLoLScSkYrmI6RJfuE+iS0DB9sHosnyPFqREQZgKItwBocLpSoG4D05zm8bQXvAe/ChsnpVixMe9WQ05afoMerGpJuPkk7M2Re8x9iwHSvrBlqGBdMopJHIqwYyLYnttnMuug8s/BxCa1p5E2Dp5IeAnWuy2EMx0RWEd1M1oh8OQe9cNR3kXtjuAQ=="
+  //default = ""
 }
 
 variable "public_subnet_cidrs" {
