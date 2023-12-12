0e5c9efb1c5fa1f06889855c19647b19583e059b
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2023-12-12 17:31:28 -0300

    Create export.py

---
+++ src/excel-export/export.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import psycopg2
+
+# Connect to the database
+connection = psycopg2.connect("postgresql://admin123:admin123@db-chatbot.cvttljzzyn1u.us-east-1.rds.amazonaws.com:5432/postgres")
+
+cursor = connection.cursor()
+
+# Execute SQL query
+query = "SELECT * FROM User"
+cursor.execute(query)
+
+# Fetch data
+data = cursor.fetchall()
+
+# Transform data using pandas DataFrame
+df = pd.DataFrame(data, columns=['Column1'])
+
+# Create Excel file
+excel_file_path = '/home/felipe/Documents/GitHub/grupo4/src/excel_export/output_file.xlsx'
+df.to_excel(excel_file_path, index=False)
+
+# Close the database connection
+cursor.close()
+connection.close()
