8ddb7695e41a1becff10a8f9ce4486294254f95f
Elisa Flemer <elisaflemer@gmail.com>
2023-12-19 14:24:55 -0300

    CRUD users

--- src/admin-interface/app/components/Users.jsx
+++ src/admin-interface/app/components/Users.jsx
@@ -2,14 +2,11 @@
 import React, { useState, useEffect } from "react";
 
 const Users = () => {
-  const [users, setUserData] = useState([
-   
-  ]);
+  const [users, setUserData] = useState([]);
+  const [editingId, setEditingId] = useState(null);
 
-  // Function to fetch history data (replace with your actual data fetching logic)
   const fetchUsers = async () => {
     try {
-      // Replace this with your API endpoint or data source
       const response = await fetch("http://localhost:5000/users");
       const data = await response.json();
       setUserData(data);
@@ -18,8 +15,49 @@ const Users = () => {
     }
   };
 
+  const handleDoubleClick = (id) => {
+    setEditingId(id);
+  };
+
+  const handleBlur = async (id, field, value) => {
+    try {
+      // Update the user data on the server using a PUT request
+      await fetch(`http://localhost:5000/users/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ [field]: value }),
+      });
+      // Fetch updated user data after the edit
+      fetchUsers();
+    } catch (error) {
+      console.error(`Error updating ${field} for user ${id}:`, error);
+    } finally {
+      setEditingId(null);
+    }
+  };
+
+  const handleRoleChange = async (id, value) => {
+    try {
+      // Update the role for the user on the server using a PUT request
+      await fetch(`http://localhost:5000/users/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ role: value }),
+      });
+      // Fetch updated user data after the edit
+      fetchUsers();
+    } catch (error) {
+      console.error(`Error updating role for user ${id}:`, error);
+    } finally {
+      setEditingId(null);
+    }
+  };
+
   useEffect(() => {
-    // Fetch history data when the component mounts
     fetchUsers();
   }, []);
 
@@ -27,7 +65,7 @@ const Users = () => {
     <div className="border-lg h-full overflow-y-auto p-4 shadow-md border-gray-100 border-[2px] rounded-md mx-4">
       <span className="flex justify-between m-2">
         <h1 className="text-2xl font-semibold mb-4">Usuários</h1>
-        <button className=" border-green-400 border-[1px] text-green-400 rounded-md py-0 px-2">
+        <button className="border-green-400 border-[1px] text-green-400 rounded-md py-0 px-2">
           BAIXAR
         </button>
       </span>
@@ -43,9 +81,55 @@ const Users = () => {
         <tbody>
           {users.map((item) => (
             <tr key={item.id} className="border-b text-center">
-              <td className="py-2 px-4">{item.name}</td>
-              <td className="py-2 px-4">{item.cellPhone}</td>
-              <td className="py-2 px-4">{item.role}</td>
+              <td
+                className="py-2 px-4 cursor-pointer"
+                onDoubleClick={() => handleDoubleClick(item.id)}
+              >
+                {editingId === item.id ? (
+                  <input
+                    type="text"
+                    defaultValue={item.name}
+                    onBlur={(e) =>
+                      handleBlur(item.id, "name", e.target.value)
+                    }
+                  />
+                ) : (
+                  item.name
+                )}
+              </td>
+              <td
+                className="py-2 px-4 cursor-pointer"
+                onDoubleClick={() => handleDoubleClick(item.id)}
+              >
+                {editingId === item.id ? (
+                  <input
+                    type="text"
+                    defaultValue={item.cellPhone}
+                    onBlur={(e) =>
+                      handleBlur(item.id, "cellPhone", e.target.value)
+                    }
+                  />
+                ) : (
+                  item.cellPhone
+                )}
+              </td>
+              <td
+                className="py-2 px-4 cursor-pointer"
+                onDoubleClick={() => handleDoubleClick(item.id)}
+              >
+                {editingId === item.id ? (
+                  <select
+                    defaultValue={item.role}
+                    onBlur={(e) => handleRoleChange(item.id, e.target.value)}
+                  >
+                    <option value="ADMIN">ADMIN</option>
+                    <option value="USER">USER</option>
+                    <option value="LEAD">LEAD</option>
+                  </select>
+                ) : (
+                  item.role
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
