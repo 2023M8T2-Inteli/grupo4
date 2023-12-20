6e78f3e489972aee17d4a4bb44423bcf97b3715f
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-12-20 15:42:24 -0300

    feat(microservice/chatbot): ✨ qrcode route

--- src/TEST-microservices/chatbot/src/main.ts
+++ src/TEST-microservices/chatbot/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(3000);
 }
 bootstrap();
